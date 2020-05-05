5dd4d023361c2bb15d58667c2f1c1562bfe629bf
Robin Wilken <Robin.Wilken@hmmh.de>
2020-05-05 20:10:26 +0200

    Sinus Oszillator

--- calm-scenes/Assets/src/c#/SineWave.cs
+++ calm-scenes/Assets/src/c#/SineWave.cs
@@ -4,13 +4,17 @@ public class SineWave : MonoBehaviour
 {
     private float amplitude;
     private float periode;
+    private float phase;
 
-    public SineWave(float amplitude, float periode) {
+    public SineWave(float amplitude, float periode, float phase) {
         this.amplitude = amplitude;
         this.periode = periode;
+        this.phase = phase;
     }
 
     public float getWave() {
-        return amplitude * Mathf.Sin(2 * Mathf.PI * (1 / periode) * Time.time);
+        return amplitude * Mathf.Sin(2 * Mathf.PI * (1 / periode) * Time.time + phase);
     }
+
+
 }
--- calm-scenes/Assets/src/c#/scenes/Scene01.cs
+++ calm-scenes/Assets/src/c#/scenes/Scene01.cs
@@ -4,9 +4,21 @@ public class Scene01 : MonoBehaviour
 {
     public float wave;
     public SineWave sineWave;
+    GameObject cube0;
+    GameObject cube1;
+    GameObject cube2;
+
+    void Start()
+    {
+        cube0 = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        cube1 = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        cube2 = GameObject.CreatePrimitive(PrimitiveType.Cube);
+    }
 
     void Update()
     {
-        wave = new SineWave(1f, 1f).getWave();
+        wave = new SineWave(1f, 1f, 0f).getWave();
+        cube0.transform.position = Vector3.up * new SineWave(1f, 1f, 0f).getWave();
+        cube1.transform.position = Vector3.up * new SineWave(1f, 1f, 1f).getWave() + Vector3.right;
     }
 }
